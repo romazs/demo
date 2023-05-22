5e4b9640c69ce198bb0a9e80b44f3c3371308272
Roma <114018895+romazs@users.noreply.github.com>
2023-05-22 13:45:40 +0300

    Update project2.py

--- project2.py
+++ project2.py
@@ -2,7 +2,6 @@ import time
 import pytest
 
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
